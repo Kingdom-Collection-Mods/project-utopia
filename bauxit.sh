962e421f7a173a9cec70f3d9f7b79a9a00441c2e
Freddy <youfreddi@gmail.com>
2025-12-21 17:27:53 +0100

    Bauxite is now also spawned by lead but doesn't combine with sulfur spawns, instead max value is chosen

--- bauxit.sh
+++ bauxit.sh
@@ -17,17 +17,51 @@ for file in "$directory"/*; do
         # Remove existing bg_bauxite_mining lines
         sed -i '/^\s*building_bauxite_mine = .*/d' "$file"
 
-        # Add new bg_bauxite_mining lines based on building_sulfur_mine
-        awk '
-            /building_sulfur_mine = [0-9]+/ {
-                match($0, /building_sulfur_mine = ([0-9]+)/, arr)
-                sulfur_value = arr[1]
-                bauxite_value = int((sulfur_value + 1) / 2)
-                print $0
-                printf "        building_bauxite_mine = %d \n", bauxite_value, sulfur_value
-                next
+       awk '
+        function ceil_half(x) { return int((x + 1) / 2) }
+
+        {
+            line = $0
+
+            # Track when we are inside capped_resources block
+            if ($0 ~ /^\s*capped_resources\s*=\s*{/) {
+            in_capped = 1
+            inserted = 0
+            sulfur = -1
+            lead   = -1
             }
-            { print }
+
+            if (in_capped) {
+            if (match($0, /building_sulfur_mine\s*=\s*([0-9]+)/, a)) sulfur = a[1]
+            if (match($0, /building_lead_mine\s*=\s*([0-9]+)/, b))   lead   = b[1]
+
+            # Print the line as-is
+            print line
+
+            # If this line is sulfur or lead and we have not inserted yet,
+            # insert ONE bauxite line based on max(sulfur, lead) if known so far.
+            if (!inserted && ($0 ~ /building_sulfur_mine\s*=/ || $0 ~ /building_lead_mine\s*=/)) {
+                maxv = sulfur
+                if (lead > maxv) maxv = lead
+
+                # If at least one of them exists, insert
+                if (maxv >= 0) {
+                printf "        building_bauxite_mine = %d\n", ceil_half(maxv)
+                inserted = 1
+                }
+            }
+
+            # End of capped_resources block
+            if ($0 ~ /^\s*}\s*$/) {
+                in_capped = 0
+            }
+
+            next
+            }
+
+            # Outside capped_resources, just print
+            print line
+        }
         ' "$file" > "${file}.tmp" && mv "${file}.tmp" "$file"
 
         echo "Finished processing $file."
